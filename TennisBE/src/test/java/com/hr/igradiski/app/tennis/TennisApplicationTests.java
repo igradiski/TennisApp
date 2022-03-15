26db8511bfdba6781ceb957ae7ac98a36172e4c1
DESKTOP-0OQ324N\Ivor <gradiski.zrinski.ivor@gmail.com>
2022-03-15 21:07:10 +0100

    Initial commmit

---
+++ TennisBE/src/test/java/com/hr/igradiski/app/tennis/TennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.igradiski.app.tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
